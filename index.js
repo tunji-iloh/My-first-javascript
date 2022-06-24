8e0f30a2dae8b9861d5a13a05eceb427c939efec
tunji-iloh <105460677+tunji-iloh@users.noreply.github.com>
2022-06-24 09:56:56 +0100

    My first javascript commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+        var my_name = ' iloh olatunji olasunkanmi'
+        var my_height = ' 5.8ft'
+        var my_country = ' Nigeria'
+
+        str1 = 'my name is' + my_name
+        str2 = 'my height is' + my_height
+        str3 = 'my country is' + my_country
+
+        alert(
+                str1 + '\n' + str2 + '\n' + str3
+        )
